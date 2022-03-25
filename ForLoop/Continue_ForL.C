183887e2abfe3297d8dd877844d112ff39d29490
Vishalpatil6 <101865355+Vishalpatil6@users.noreply.github.com>
2022-03-25 17:33:57 +0530

    ADDED: Continue Statement  Using For Loop

---
+++ ForLoop/Continue_ForL.C
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+    int i;
+    clrscr();
+
+    for (i = 1; i <= 10; i++)
+    {
+        if (i == 5)
+        {
+            continue;
+        }
+        printf("  %d", i);
+    }
+
+    getch();
+}
