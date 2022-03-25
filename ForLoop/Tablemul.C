235cd9118453e3dc49729b953c9a9649856cdd04
Vishalpatil6 <101865355+Vishalpatil6@users.noreply.github.com>
2022-03-25 09:29:40 +0530

    ADDED:Program to print table of a number using functions

---
+++ ForLoop/Tablemul.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+
+  int n,i,r;
+  printf("Enter a no for table: ");
+  scanf("%d",&n);
+  for(i=1;i<=10;i++)
+  {
+    r=n*i;
+    printf("%dx%d=%d\n",n,i,r);
+  }
+
+ getch();
+} 
+
