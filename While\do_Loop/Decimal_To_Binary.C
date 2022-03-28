bd0bbf6b8c354fccc2113efa0349a3e08c70cfee
Vishalpatil6 <101865355+Vishalpatil6@users.noreply.github.com>
2022-03-28 21:43:50 +0530

    ADDED: [Vishal] Convert Number Decimal To Binary

---
+++ While\do_Loop/Decimal_To_Binary.C
@@ -0,0 +1,24 @@
+include <stdio.h>  
+ void main()
+ {
+     long num, decimal_num, remainder, base = 1, binary = 0, no_of_1s = 0;
+  
+     printf("Enter a decimal integer:");
+     scanf("%ld", &num);
+     decimal_num = num;
+     while (num > 0)
+     {
+         remainder = num % 2;
+         /*  To count no.of 1s */
+         if (remainder == 1)
+         {
+             no_of_1s++;
+         }
+         binary = binary + remainder * base;
+         num = num / 2;
+         base = base * 10;
+     }
+     printf("\nInput number is = %d", decimal_num);
+     printf("\nIts binary equivalent is = %ld", binary);
+     printf("\nNo.of 1's in the binary number is = %d", no_of_1s);
+ }
