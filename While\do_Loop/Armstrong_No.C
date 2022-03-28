a32d12f0c0585c1a10dd7d67b371de1dcd2a3a6b
Vishalpatil6 <101865355+Vishalpatil6@users.noreply.github.com>
2022-03-28 21:30:41 +0530

    ADDED: [Vishal] Check Given  Number Armstrong Or Not

---
+++ While\do_Loop/Armstrong_No.C
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int n,s,sum=0,temp;
+	clrscr();
+	printf("n::");
+	scanf("%d",&n);
+	temp=n;
+	while(n>0)
+	{
+		s=n%10;
+		sum=sum+(s*s*s);
+		n=n/10;
+	}
+	if(sum==temp)
+	{
+ 		printf("Number Is Armstrong");
+	}
+	else
+	{
+ 	 	printf("Number Is Not Armstrong");
+	}
+	getch();
+}
