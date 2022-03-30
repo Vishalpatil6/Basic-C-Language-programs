532203cde476140a7073f3ddb7be587d59a8d59c
Vishalpatil6 <101865355+Vishalpatil6@users.noreply.github.com>
2022-03-30 10:52:55 +0530

    ADDED: [Vishal] Find Maximum Number  From Array

---
+++ Find_Max.C
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int num[10],i,max=0,n;
+	clrscr();
+	printf("How many elements?\n");
+	scanf("%d",&n);
+	printf("Enter %d elements\n",n);
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&num[i]);
+		if(num[i]>max)
+		{
+			max=num[i];
+		}
+	}
+	printf("maximum=%d",max);
+	getch();
+}
