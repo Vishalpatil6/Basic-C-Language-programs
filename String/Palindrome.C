61689ecd525b08f4b29e2f9f11094ae706704fdd
Vishalpatil6 <101865355+Vishalpatil6@users.noreply.github.com>
2022-03-30 21:46:58 +0530

    ADDED: [Vishal] Checking String Palindrome Or Not

---
+++ String/Palindrome.C
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <conio.h>
+#include <string.h>
+void main()
+{
+    char s[20];
+    int i, j, flag = 0;
+    clrscr();
+    printf("Enter string:");
+    gets(s);
+    for (i = 0, j = strlen(s) - 1; i < strlen(s) / 2; i++, j--)
+    {
+        if (s[i] != s[j])
+        {
+            flag = 1;
+            break;
+        }
+    }
+    if (flag == 0)
+        printf("\nPalindrome");
+    else
+        printf("\nNot palindrome");
+    getch();
+}
